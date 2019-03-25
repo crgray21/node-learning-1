14df982d0b8deff95ac910475e7e68a72561f5de
Cory <crgray21@yahoo.com>
2019-03-25 19:57:08 -0400

    File writing and reading for .json file

---
+++ models/product.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            try {
+                cb(JSON.parse(fileContent));    
+            } catch (error) {
+                cb([]);
+            }  
+        }
+    });
+};
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        });
+    }
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+};
