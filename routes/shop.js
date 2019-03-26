85ce2b9bade3731d087455f86884e5dce649a492
Cory <crgray21@yahoo.com>
2019-03-25 20:09:08 -0400

    Added additonal ejs templates and routes

--- routes/shop.js
+++ routes/shop.js
@@ -6,4 +6,10 @@ const router = express.Router();
 
 router.get('/', productsController.getProducts);
 
+router.get('/products');
+
+router.get('/cart');
+
+router.get('/checkout');
+
 module.exports = router;
